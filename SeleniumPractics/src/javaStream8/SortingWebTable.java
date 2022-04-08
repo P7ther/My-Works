8b9f0997e21852129be919fa1fc8554b82a21891
Satyajit <p7thres@gmail.com>
2022-04-08 17:55:33 +0530

    Updated Code

--- SeleniumPractics/src/javaStream8/SortingWebTable.java
+++ SeleniumPractics/src/javaStream8/SortingWebTable.java
@@ -24,23 +24,36 @@ public class SortingWebTable
 		//Click on the column
 		driver.findElement(By.xpath("//tr/th[1]")).click();
 		//Capture all webelements into list
-		List<WebElement>elements=driver.findElements(By.xpath("//tr/th[1]"));
+		List<WebElement>elements=driver.findElements(By.xpath("//tr/td[1]"));
 		//capture text of all webelements into new(original) list
 		List<String>orginalList=elements.stream().map(s->s.getText()).collect(Collectors.toList());
 		//sort on the orginal list of step 3
 		List<String>ShortedList=orginalList.stream().sorted().collect(Collectors.toList());
 		//compare orginal list vs sorted list
 		Assert.assertTrue(orginalList.equals(ShortedList));
-		System.out.println("Working Perfectly");
+		
+		List<String>price;
+		//Scan the name column with getText->Beans->print the price of the rice
+		do
+		{
+		List<WebElement> rows = driver.findElements(By.xpath("//tr/td[1]"));
+		price=rows.stream().filter(s->s.getText().contains("Rice"))
+				.map(s->getVagePrice(s)).collect(Collectors.toList());
+		price.forEach(a->System.out.println(a));
+		
+		if(price.size()<1) 
+		{
+			driver.findElement(By.cssSelector("[aria-label='Next']")).click();
+		}
+		}while(price.size()<1);
 		
 		
-		
-		
-		
-		
-		
-		
-		
+	}
+
+	private static String getVagePrice(WebElement s) 
+	{
+		String pricevalue=s.findElement(By.xpath("following-sibling::td[1]")).getText();		
+		return pricevalue;
 	}
 
 }
